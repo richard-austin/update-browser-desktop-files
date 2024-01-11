3f7f5743b0cd6c42044b6eff2ee6947d0f87ddfc
richard-austin <richard.david.austin@gm>
2024-01-11 15:07:59 +0000

    Script working for Brave, Chromium, Google Chrome, Google Chrome Beta and MS Edge.

---
+++ update-desktop-files.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+declare -a desktopFiles=("/var/lib/snapd/desktop/applications/brave_brave.desktop"
+                         "/var/lib/snapd/desktop/applications/chromium_chromium.desktop"
+                         "/usr/share/applications/google-chrome-beta.desktop"
+                         "/usr/share/applications/google-chrome.desktop"
+                         "/usr/share/applications/microsoft-edge.desktop")
+params="--enable-features=VaapiVideoDecoder,VaapiVideoEncoder,VaapiVideoDecodeLinuxGL"
+
+for input in "${desktopFiles[@]}"
+do
+  tmpFile=$input.tmp
+  printf "" > $tmpFile
+  while read -r line
+    do
+      if [[ $line == Exec*  &&  $line != *$params ]]
+      then
+          echo $line" "$params >> $tmpFile
+      else
+          echo $line >> $tmpFile
+      fi
+  done < "$input"
+  cp $tmpFile $input
+  rm $tmpFile
+done
+
